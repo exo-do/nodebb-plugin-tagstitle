093149ab2f032f54fd1f33879ccbcbff4988b92e
Rubén Ballester <irbbau@gmail.com>
2015-01-10 17:59:59 +0100

    NodeBB Plugin - TagsTitle update 10/01/2014

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # NodeBB Tags Title Plugin
 
-Este plugin otorga la funcionalidad de atribuir características especiales a los temas creados por los usuarios mediante la escritura de una serie de tags concretos. Por ejemplo, para hacer un post privado podemos escribir +hd, para un post no apto para el trabajo podemos escribir +nsfw
+Este plugin otorga la funcionalidad de atribuir características especiales a los temas creados por los usuarios mediante la escritura de una serie de tags concretos. Por ejemplo, para hacer un post privado podemos escribir +HD, para un post no apto para el trabajo podemos escribir +NSFW
 
-Acepta mayúsculas, minúsculas, con +, con + y sin nada también, por si te sabía a poco (revolucionario)
+Acepta mayúsculas, minúsculas y combinaciones de ambas. Las etiquetas incluidas por el momento son:
+
++HD : Usuario registrado y con 1 mensaje o más
++18 : Usuario registrado y con 1 mensaje o más
++NSFW : Usuario registrado y con 1 mensaje o más
++PRV : Usuario registrado y con una reputación de 10 punto o más.
+
+
+##Funcionamiento y compatibilidad
+
+El plugin está funcionando correctamente en la rama de NodeBB 0.6.X.
+
+El plugin hace uso de elementos del core de NodeBB, concretamente "user.js"
+
+El plugin hace uso de la plantilla topic-error.tpl, que es la que hay que editar y en la que aparecen los errores.
+
+Compatible con la API
 
 
 ## Instalación
@@ -10,6 +26,21 @@ Acepta mayúsculas, minúsculas, con +, con + y sin nada también, por si te sab
     Crea la carpeta "nodebb-plugin-tagstitle", introduce los archivos e introduce la carpeta  dentro de "node_modules"
     Ejecuta en la terminal "npm install"
 
-## Problemas conocidos
+## Problemas conocidos y características pendientes
 
-Todavía hace falta realizar las redirecciones o cambiar los archivos TPL.
+Todavía hace falta realizar las redirecciones a las rutas definidas del error del topic.
+
+##Como añadir una nueva etiqueta al plugin
+
+Para añadir una nueva etiqueta al plugin copia y pega después de ''//Añade a partir de aquí las nuevas etiquetas adicionales''
+
+El siguiente código:
+
+    else if(topicTitle.indexOf('+etiqueta')  >= 0) {
+      //Contiene la etiqueta +etiqueta
+      if (userid <= 1 || tagsTitle.postCount  == 0 ) {
+        //Redireccionar
+      }
+    }
+
+Donde el nombre de la etiqueta es el que aparece después de index.Of
--- library.js
+++ library.js
@@ -1,72 +1,62 @@
 "use strict";
+
 var User = module.parent.require('./user');
 
 var tagsTitle = {};
-	tagsTitle.getTopic = function (postContent, callback) {
 
+  tagsTitle.init = function(params, callback) {
+    var middleware = params.middleware,
+    controllers = params.controllers;
+    controllers.getErrorPage = function (req, res, next) {
+      //Renderiza la plantilla topic-error en el futuro enlace del foro //servidor.com/topicerror
+      res.render('topic-error', {});
+    };
+    //Creamos las direcciones //servidor.com/topicerror mediante params.router.get
+    params.router.get('/topicerror',middleware.buildHeader, controllers.getErrorPage);
+    params.router.get('/api/topicerror', controllers.getErrorPage);
+    callback();
+  };
+
+  tagsTitle.getTopic = function (postContent, callback) {
     //Obtenemos los datos del usuario
-
-		var userid = postContent.uid;
+    var userid = postContent.uid;
     var userdata = User.getUserData(userid, function(err,getUserData) {
-       //Introducimos todos los datos del usuario en tagsTitle
-       tagsTitle.postCount = getUserData['postcount'];
-       tagsTitle.reputation = getUserData['reputation'];
+    //Introducimos todos los datos del usuario en tagsTitle
+      tagsTitle.postCount = getUserData.postcount;
+      tagsTitle.reputation = getUserData.reputation;
     });
-
-  //Metemos en una variable el título en minúscula
-
- 	var topicTitle = postContent['title'].toLowerCase();
-
-  //Comprobamos si hay etiquetas insertadas por el usuario en el título
-
-    if(topicTitle.indexOf('hd')  >= 0) {
+    //Metemos en una variable el título en minúscula
+    var topicTitle = postContent.title.toLowerCase();
+    //Comprobamos si hay etiquetas insertadas por el usuario en el título
+    if(topicTitle.indexOf('+hd')  >= 0) {
       //Contiene la etiqueta +HD
-      if (userid > 0 & tagsTitle.postCount  > 0 ) {
-      //No hacemos nada
-      }
-      else {
-      //Redireccionar
+      if (userid <= 1 || tagsTitle.postCount  == 0 ) {
+        //Redireccionar
 
-      }
     }
-    else if(topicTitle.indexOf('18')  >= 0) {
+  }
+    else if(topicTitle.indexOf('+18')  >= 0) {
       //Contiene la etiqueta +18
-      if (userid > 0 & tagsTitle.postCount  > 0 ) {
-      //No hacemos nada
-
-      }
-      else {
-      //Redireccionar
+      if (userid <= 1 || tagsTitle.postCount  == 0 ) {
+        //Redireccionar
       }
     }
 
-    else if(topicTitle.indexOf('prv')  >= 0) {
+    else if(topicTitle.indexOf('+prv')  >= 0) {
       //Contiene la etiqueta +PRV
-            if (userid > 0 & tagsTitle.reputation > 10  ) {
-      //No hacemos nada
-
-      }
-      else {
-      //Redireccionar
+      if (userid <= 1 || tagsTitle.reputation <= 10  ) {
+        //Redireccionar
       }
     }
-    else if(topicTitle.indexOf('nsfw')  >= 0) {
+    else if(topicTitle.indexOf('+nsfw')  >= 0) {
       //Contiene la etiqueta +nsfw
-            if (userid > 0 & tagsTitle.postCount  > 0 ) {
-      //No hacemos nada
-
-      }
-      else {
-      //Redireccionar
+      if (userid <= 1 || tagsTitle.postCount  == 0 ) {
+        //Redireccionar
       }
     }
-    else {
-      //No contiene nada
-    }
-  //Dejamos los datos tal cual están
-  console.log(tagsTitle.postCount);
-	postContent = postContent;
-	callback(null, postContent);
-	};
-
+    //Añade a partir de aquí las nuevas etiquetas adicionales
+    
+    //Fin de las etiquetas adicionales
+    callback(null, postContent);
+  };
 module.exports = tagsTitle;
